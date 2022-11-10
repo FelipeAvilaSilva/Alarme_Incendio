fd1d5f110ef1bbdce9b52d3878a5654b0ddb8fe2
FelipeAvilaSilva <fasilva@inf.ufpel.edu.br>
2022-11-10 01:03:33 -0300

    init
    add dht11

---
+++ OTAA_OLED/OTAA_OLED.ino
@@ -0,0 +1,110 @@
+/*
+ * HelTec Automation(TM) LoRaWAN 1.0.2 OTAA example use OTAA, CLASS A
+ *
+ * Function summary:
+ *
+ * - use internal RTC(150KHz);
+ *
+ * - Include stop mode and deep sleep mode;
+ *
+ * - 15S data send cycle;
+ *
+ * - Informations output via serial(115200);
+ *
+ * - Only ESP32 + LoRa series boards can use this library, need a license
+ *   to make the code run(check you license here: http://www.heltec.cn/search );
+ *
+ * You can change some definition in "Commissioning.h" and "LoRaMac-definitions.h"
+ *
+ * HelTec AutoMation, Chengdu, China.
+ * 成都惠利特自动化科技有限公司
+ * https://heltec.org
+ * support@heltec.cn
+ *
+ *this project also release in GitHub:
+ *https://github.com/HelTecAutomation/ESP32_LoRaWAN
+ *
+ *
+ * To use this on Thethingsnetwork.org add the following Payload Formatter for this device
+*/
+
+#include <ESP32_LoRaWAN.h>
+#include "Arduino.h"
+#include "OTAA_OLED_KEYS.h"
+
+
+static void prepareTxFrame( uint8_t port )
+{
+    // Prepare upstream data transmission at the next possible time.
+  
+	
+    appDataSize = 4;//AppDataSize max value is 64
+    // Format the data to bytes	
+    appData[0] = 1;
+    appData[1] = 0;
+    appData[2] = 0;
+    appData[3] = 1;	
+}
+
+// Add your initialization code here
+void setup()
+{
+  if(mcuStarted==0)
+  {
+    LoRaWAN.displayMcuInit();
+  }
+  Serial.begin(115200);
+  while (!Serial);
+  SPI.begin(SCK,MISO,MOSI,SS);
+  Mcu.init(SS,RST_LoRa,DIO0,DIO1,license);
+  deviceState = DEVICE_STATE_INIT;
+}
+
+// The loop function is called in an endless loop
+void loop()
+{
+  switch( deviceState )
+  {
+    case DEVICE_STATE_INIT:
+    {
+#if(LORAWAN_DEVEUI_AUTO)
+			LoRaWAN.generateDeveuiByChipID();
+#endif
+      LoRaWAN.init(loraWanClass,loraWanRegion);
+      break;
+    }
+    case DEVICE_STATE_JOIN:
+    {
+      LoRaWAN.displayJoining();
+      LoRaWAN.join();
+      break;
+    }
+    case DEVICE_STATE_SEND:
+    {
+      LoRaWAN.displaySending();
+      prepareTxFrame( appPort );
+      LoRaWAN.send(loraWanClass);
+      deviceState = DEVICE_STATE_CYCLE;
+      break;
+    }
+    case DEVICE_STATE_CYCLE:
+    {
+      // Schedule next packet transmission
+      txDutyCycleTime = appTxDutyCycle + randr( -APP_TX_DUTYCYCLE_RND, APP_TX_DUTYCYCLE_RND );
+      LoRaWAN.cycle(txDutyCycleTime);
+      deviceState = DEVICE_STATE_SLEEP;
+      break;
+    }
+    case DEVICE_STATE_SLEEP:
+    {
+      LoRaWAN.displayAck();
+      LoRaWAN.sleep(loraWanClass,debugLevel);
+      break;
+    }
+    default:
+    {
+      deviceState = DEVICE_STATE_INIT;
+      break;
+    }
+  }
+}
